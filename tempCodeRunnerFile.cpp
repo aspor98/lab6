e045fbea13797d671fee3c09cac8dfbeeb47ab21
aspor <tikomporn_luxs@cmu.ac.th>
2022-12-09 17:04:00 +0700

    lab6_2.cpp completed

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,12 @@
-cout<<odd;
+return ycomp;
+}
+double pythagoras(double xcomp,double ycomp){
+   double result_vec_length;
+   result_vec_length = sqrt(pow(xcomp,2)+pow(ycomp,2));
+   return result_vec_length;
+}
+double showResult(double result_vec_length,double result_vec_direction){
+
+    cout << "Length of the resultant vector = " << result_vec_length << "\n";
+    cout <<  "Direction of the resultant vector (deg) = " << result_vec_direction;
+}
