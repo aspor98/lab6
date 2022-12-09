9b1a6127b3fa98259f380ce4babfa0ee3df62c95
aspor <tikomporn_luxs@cmu.ac.th>
2022-12-09 16:01:55 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num,even = 0,odd = 0;
+
+    do{cout << "Enter an integer: ";
+    cin>> num;
+    if(num%2==0 && num!=0){
+        even += 1;
+    
+        
+    }else if(num%2==1 && num != 0){
+        odd += 1;
+    
+        
+    }
+    }while(num !=0);
+    
+    
+
+    cout << "#Even numbers = "<< even << "\n";
+    cout << "#Odd numbers = " << odd;
+    
     return 0;
 }
